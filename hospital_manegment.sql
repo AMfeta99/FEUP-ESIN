19f6870f75064fa6afc956a8eb39ef8061f49518
AMfeta99 <74252797+AMfeta99@users.noreply.github.com>
2020-11-15 18:29:56 +0000

    Sql-(tabelas/relações)
    Construção de tabelas e relações. Inserção de alguns dados

---
+++ hospital_manegment.sql
@@ -0,0 +1,203 @@
+
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE Department(
+    number integer PRIMARY KEY,
+    name text NOT NULL,
+    total_beds integer NOT NULL
+    --- n_beds_available= total_beds- (SELECT)--- acabar
+);
+
+CREATE TABLE Patient (
+	cc integer PRIMARY KEY,
+	name text NOT NULL,
+    age integer NOT NULL,
+    phone_number integer,
+	mail_address text NOT NULL,
+    password text NOT NULL
+);
+
+CREATE TABLE Doctor (
+	id integer PRIMARY KEY AUTOINCREMENT, 
+	name text NOT NULL,
+    photo BLOB -- não sei como por ==> aula min 25
+    phone_number integer,
+	mail_address text NOT NULL,
+    password text NOT NULL,
+    speciality integer NOT NULL REFERENCES Department
+);
+
+CREATE TABLE Nurse (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name text NOT NULL,
+    phone_number integer NOT NULL,
+	mail_address text ,
+    password text NOT NULL,
+    department integer NOT NULL REFERENCES Department
+);
+
+CREATE TABLE Block_time (
+	code integer PRIMARY KEY,
+    begin_time Date NOT NULL, -- not sure if the type is datetime --> acho q é text
+    end_time Date NOT NULL,
+    week_day text NOT NULL CHECK (week_day = "MON" OR week_day = "TUE" OR week_day = "WED" OR week_day = "THU" OR week_day = "FRI" ),
+    CHECK (Date(begin_time) < Date(end_time))
+    
+);
+
+CREATE TABLE Reservation(
+    id integer PRIMARY KEY AUTOINCREMENT, 
+    date Date NOT NULL, -- tipo 11-11-2021 --- não sei se é assim!
+    time integer NOT NULL REFERENCES Block_time, -- the primary key of block time is an integer
+    doctor integer NOT NULL REFERENCES Doctor, 
+    patient integer NOT NULL REFERENCES Patient
+    
+);
+
+CREATE TABLE Appointment(
+    id integer PRIMARY KEY, 
+    date Date NOT NULL, -- tipo 11-11-2021
+    time integer NOT NULL REFERENCES Block_time, -- the primary key of block time is an integer
+    doctor integer NOT NULL REFERENCES Doctor, 
+    patient integer NOT NULL REFERENCES Patient
+    
+);
+
+CREATE TABLE Prescription (
+    id integer PRIMARY KEY, 
+    date Date NOT NULL, -- tipo 11-11-2021
+    date_limit Date NOT NULL, 
+    id_appointment integer NOT NULL REFERENCES Appointment,
+    CHECK (Date(date_limit)> Date(date))
+);
+
+CREATE TABLE Medicine (
+    code integer PRIMARY KEY, 
+    name text NOT NULL, -- paracetamol
+    dose text NOT NULL, -- 1000 mg, 500 mg ....
+    instructions text NOT NULL
+);
+
+CREATE TABLE Disease(
+    name text PRIMARY KEY
+);
+
+CREATE TABLE AppointmentDiagnosis  (
+    id_appointment integer REFERENCES Appointment,
+	disease text REFERENCES Disease,
+	PRIMARY KEY (id_appointment, disease)
+);
+
+CREATE TABLE Block_time_and_Doctor(
+    block_time integer REFERENCES Block_time,
+    doctor integer REFERENCES Doctor,
+    PRIMARY KEY (block_time, doctor)
+);
+
+CREATE TABLE PrescriptionOfMedicine(
+    id_prescription integer REFERENCES Prescription,
+    id_medicine integer REFERENCES Medicine,
+    quantity integer CHECK (quantity >=1),
+    PRIMARY KEY (id_prescription, id_medicine)
+);
+
+CREATE TABLE ReceiveNotification (
+    id integer,
+    doctor integer REFERENCES Doctor,
+    patient integer REFERENCES Patient,
+    message text NOT NULL CHECK (message ="reservation accept" OR message = "reservation denied"),
+    PRIMARY KEY (id, doctor, patient)
+);
+
+CREATE TABLE Report (
+    id integer PRIMARY KEY,
+    date Date NOT NULL,
+    message text NOT NULL
+);
+
+CREATE TABLE Bed(
+    number integer PRIMARY KEY,
+    id_department integer NOT NULL REFERENCES Department
+);
+
+CREATE TABLE Inpatient(
+    code integer PRIMARY KEY,
+    visiting_hours text,
+    patient integer REFERENCES Patient,
+    daily_report integer REFERENCES Report,
+    bed integer REFERENCES Bed,
+    doctor integer REFERENCES Doctor
+
+);
+
+CREATE TABLE MedicationAdministered(
+    code_medicine integer REFERENCES Medicine,
+    inpatient integer REFERENCES Inpatient,
+    PRIMARY KEY(code_medicine, inpatient)
+);
+
+CREATE TABLE NursesOfInpatient(
+    inpatient integer REFERENCES Inpatient,
+    nurse integer REFERENCES Nurse,
+    PRIMARY KEY(nurse, inpatient)
+);
+
+--acho q aquilo do medico e departmento tem q ser assim e talvez fazer um check para ser pelo menos 1 (não sei ) 
+CREATE TABLE Department_Doctor(
+    number integer REFERENCES Department,
+    doctor integer REFERENCES Doctor,
+    PRIMARY KEY (number, doctor)
+);
+
+--Inserir dados:
+
+--Department  (não faço a minima ideia nas do nºd camas)
+INSERT INTO Department (number,name, total_beds) VALUES (1,'Cardiology', 20);
+INSERT INTO Department (number,name, total_beds) VALUES (2,'Surgery', 25);
+INSERT INTO Department (number,name, total_beds) VALUES (3,'Dermatology', 15);
+INSERT INTO Department (number,name, total_beds) VALUES (4,'Orthopedics', 40);
+INSERT INTO Department (number,name, total_beds) VALUES (5,'Pediatrics', 38);
+INSERT INTO Department (number,name, total_beds) VALUES (6,'Pulmonology', 20);
+INSERT INTO Department (number,name, total_beds) VALUES (7,'Psychiatry', 0);
+INSERT INTO Department (number,name, total_beds) VALUES (8,'Radiology', 0);
+INSERT INTO Department (number,name, total_beds) VALUES (9,'Urology', 33);
+INSERT INTO Department (number,name, total_beds) VALUES (10,'Rheumatology', 15);
+
+--Patient  (depois podemos acrescentar mais)
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (15991790,'Ana Marta Silva', 25, 926524200, 'AMarta@gmail.com', 'amart25');
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (14511630,'joão Maria Pereira', 62, 917849203, 'jojo@sapo.pt', 'jmp413');
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (15726640,'Rita Faria', 78, 928423651, 'Faria@hotmail.com', 'riarita');
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (12054713,'Maria Madalena Melo', 13, 964566667, 'triM@fe.up.com', 'm1m2m3');
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (84310576,'André Fernandes Gonçalves', 17, 963634263, 'AndFerGon@gmail.com', 'gonçalvesAF');
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (15341150,'Gustavo Machado de Melo', 84, 922222222, 'Gmachado@gmail.com', 'machadomelado');
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (18886451,'Francisco Pinto de Oliveira', 37, 916161336, 'oliveira@sapo.pt', 'pipinto');
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (17213654,'Cláudia Oliveira', 49, 928425411, 'clod@hotmail.com', 'oliva49');
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (15431203,'Diogo José Mendes', 83, 967776667, 'mendes@fe.up.com', 'jandjkmds');
+INSERT INTO Patient (cc,name, age, phone_number, mail_address, password) VALUES (15798264,'Carolina de Almeida Peixoto', 96, 961010104, 'Carol@gmail.com', 'caramba96');
+-- testar casos de pacientes sem telemovel já q este não é obrigatorio
+INSERT INTO Patient (cc,name, age, mail_address, password) VALUES (15431264,'Carolina Lima', 64,'Carolima@gmail.com', 'C95Lima');
+INSERT INTO Patient (cc,name, age, mail_address, password) VALUES (18156437,'Abel valerio', 55, 'abelval@gmail.com', 'AiValerio');
+INSERT INTO Patient (cc,name, age, mail_address, password) VALUES (16351542,'Carlos Sousa', 11, 'carlitos@gmail.com', 'asdfdc42'); 
+
+
+--Doctor (tenho q ver como se insere as variaveis do tipo blob)
+-- INSERT INTO Doctor (name,photo, phone_number, mail_address, password,speciality ) VALUES ('João Sousa',??? , 966754201,'DoctorJoão@gmail.com', 'joaodopulmao', 6);
+-- INSERT INTO Doctor (name,photo, phone_number, mail_address, password,speciality) VALUES ('Ana Magalhães',??? , 966754121,'DoctorAna@gmail.com', 'AnaMag', 4);
+-- INSERT INTO Doctor (name,photo, phone_number, mail_address, password,speciality) VALUES ('Maria Machado',??? , 966451201,'DoctorMaria@gmail.com', 'MaMachado', 3);
+-- INSERT INTO Doctor (name,photo, phone_number, mail_address, password,speciality) VALUES ('Antonio Gomes',??? , 952154277,'DoctorTo@gmail.com', 'AntGomes45', 8);
+-- INSERT INTO Doctor (name,photo, phone_number, mail_address, password,speciality) VALUES ('Filipa Rocha',??? , 969954201,'DoctorPipa@gmail.com', 'FillRocha4', 5);
+-- INSERT INTO Doctor (name,photo, phone_number, mail_address, password,speciality) VALUES ('André Pereira',??? , 975462201,'DoctorAndre@gmail.com', '54631Pereira', 3);
+
+---Nurse 
+INSERT INTO Nurse (name,phone_number, mail_address, password, department) VALUES ('João Silva', 966752221,'NurseJoãoSilva@gmail.com', 'silvinha', 4);
+INSERT INTO Nurse (name,phone_number, mail_address, password, department) VALUES ('Matilde Silva', 966788821,'NurseMS@gmail.com', 'matilvaa84', 4);
+INSERT INTO Nurse (name,phone_number, mail_address, password, department) VALUES ('Teresa Amaro', 967856621,'Nursetete@gmail.com', 'amteresa', 3);
+INSERT INTO Nurse (name,phone_number, mail_address, password, department) VALUES ('Marta cerqueira', 966658881,'NurseMartaC@gmail.com', '982Marta4', 2);
+INSERT INTO Nurse (name,phone_number, mail_address, password, department) VALUES ('jose Costa', 966733211,'NursejoseC@gmail.com', 'JCosta6451', 5);
+INSERT INTO Nurse (name,phone_number, password, department) VALUES ('pedro ferreira', 924494571, 'jfjkhy', 8);
+INSERT INTO Nurse (name,phone_number, password, department) VALUES ('Paulo Pedra',963524201, 'j5431vhg', 7);
+INSERT INTO Nurse (name,phone_number, password, department) VALUES ('jorge Madureira',911204321, 'hhcjnkj2', 5);
+INSERT INTO Nurse (name,phone_number, password, department) VALUES ('Marta Pen',920014201, '543ghgkl', 5);
+INSERT INTO Nurse (name,phone_number, password, department) VALUES ('vitor carvalho',963444209, 'vitorinhoCarvalhinho', 1);
+
+
