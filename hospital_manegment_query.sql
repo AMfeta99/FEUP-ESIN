ebbcd150bcdd1653671d5afe3e036248ed0c39f8
AMfeta99 <74252797+AMfeta99@users.noreply.github.com>
2020-11-21 19:12:11 +0000

    SQL_query
    Manipulação dos dados

--- hospital_manegment_query.sql
+++ hospital_manegment_query.sql
@@ -33,3 +33,67 @@ UPDATE Department SET n_beds_available=  total_beds-n_beds_occupy;
 
 SELECT *
 FROM Department;
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+/* Login */
+-- Patients that exist in the database
+SELECT * 
+FROM Patient;
+
+-- To find out if the patient is registered it is necessary to see the names of the patients in the database
+-- Name of Patient
+SELECT name
+FROM Patient;
+
+-- To find out if the patient is registered it is necessary to check the name-password correspondence
+SELECT name, password
+FROM Patient;
+
+/* Pagina do medico sobre um paciente*/
+-- patient data
+SELECT name, age, mail_address
+FROM Patient;
+
+/* Appointment*/
+--patients appointments
+SELECT date, Block_time.begin_time as Hour, week_day, Doctor.name as Doctor, Patient.name as Patient
+FROM Appointment JOIN Doctor ON doctor=Doctor.id
+JOIN Block_time ON code=time
+JOIN Patient ON patient=Patient.cc;
+
+-- all appointments of the Doctor
+SELECT date, Block_time.begin_time as Hour, week_day , Patient.name as Patient
+FROM Appointment JOIN Patient ON patient=Patient.cc
+JOIN Block_time ON code=time
+JOIN Doctor ON doctor=Doctor.id
+WHERE doctor=5;
+
+-- all appointments of patient
+SELECT date, Block_time.begin_time as Hour, week_day , Doctor.name as Doctor, Department.name as speciality
+FROM Appointment JOIN Patient ON patient=Patient.cc
+JOIN Block_time ON code=time
+JOIN Doctor ON doctor=Doctor.id
+JOIN Department ON Doctor.speciality= Department.number
+WHERE patient=14511630;
+
+-- all appointments of Department
+SELECT date, Block_time.begin_time as Hour, week_day , Doctor.name as Doctor, Patient.name as Patient, Department.name as speciality
+FROM Appointment JOIN Patient ON patient=Patient.cc
+JOIN Block_time ON code=time
+JOIN Doctor ON doctor=Doctor.id
+JOIN Department ON Doctor.speciality= Department.number
+WHERE speciality= 5;
+
+/* Department*/
+-- all Doctors of department --falta adicionar a foto.... não mas deve haver uma forma melhor de fazer isto
+SELECT Doctor.name, Doctor.phone_number, Doctor.mail_address, Department.name as speciality
+FROM Doctor JOIN Department ON Doctor.speciality= Department.number
+WHERE speciality= 5;
+
+-- all department
+SELECT *
+FROM Department;
+
+---algumas coisas q vamos precisar tbm
+-- disease of a patient
+-- check that the patient is hospitalized
