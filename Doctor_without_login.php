4f08a2b28724b1f59800305f040406321b5d2e0e
ana <anafsferreira2@gmail.com>
2020-12-13 23:52:34 +0000

    Creation of doctor withou login page

---
+++ Doctor_without_login.php
@@ -0,0 +1,39 @@
+<?php
+  require_once('config/init.php');
+  $msg=$_SESSION["msg"] ;
+  unset($_SESSION["msg"] );
+
+  $doctor_id=$_GET['id'];
+  // require_once('config/init.php');
+  require_once('database/doctor.php');
+  try{
+    $result = getDoctorById($doctor_id); 
+    $result2 = getDepartmentOfEachDoctor($doctor_id);
+    $schedule= getDoctorSchedule($doctor_id);
+
+  } catch(PDOException $e){
+    $err = $e-> getMessage();
+    exit(0);
+  }
+
+  function canMakeAppointment($schedule, $begin_hour, $day){
+    foreach ($schedule as $row) {
+      $time =  $row["begin_time"] . ' - ' . $row["end_time"];
+      
+      
+      $begin_hour= date( "H:i", strtotime( $begin_hour) );
+      
+      if($row["week_day"] == $day && $row["begin_time"] == $begin_hour){
+          echo "Doing Appointments";
+      }
+
+    }
+  }
+?>
+
+<?php
+  include('templates/header_profile.php');
+  include('templates/doctor_info.php');
+  include('templates/footer.php');
+
+?>
---
+++ templates/doctor_info.php
@@ -0,0 +1,61 @@
+<section id="profile_pag">
+          <article>
+            <header id="dados">
+              <!-- <img class="circle" src="images/doctors/9.jpg" alt="" width="130"> -->
+              <img class="circle" src="<?php echo  $result["photo"] ?>" alt="" width="130">
+              <h4><?php echo  $result["name"] ?></h4> 
+              <h5><?php echo  $result2["Sname"] ?></h5>
+
+            </header>
+          
+            
+          </article>
+
+          <div class="info">
+          <h2>Profile</h2>
+          <span><?php echo $msg ?></span>
+            
+
+            <article class="info_section">
+                <h4 class="info_section">Contacts: </h4>
+                <h5 class="atribute">Mail Address: <p><?php echo  $result["mail_address"] ?></p></h5>
+                <?php if (strlen($result["phone_number"])>0){?>
+                <h5 class="atribute">Phone Number: <p><?php echo  $result["phone_number"]; }?></p></h5>
+            </article> 
+
+            
+            <h2>Schedule</h2>
+
+            <table id= "doctor schedule">
+                  <?php $week_days = array('MON', 'TUE', 'WED', 'THU', 'FRI');?>
+                  <?php $begin_hours = array('8:00','9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00');?>
+                  <tr> <!-- Linha 1-->
+                    <th class ="b"> Hour</th>
+                    <!-- for para correr todos os dias da semana -->
+                    <th class ="b"> Monday </th>
+                    <th class ="b"> Tuesday </th>
+                    <th class ="b"> Wednesday </th>
+                    <th class ="b"> Thursday </th>
+                    <th class ="b"> Friday </th>
+                  
+                  </tr>
+                  <?php foreach($begin_hours as $begin_hour){ ?> <!-- Para cada linha-->
+                    <tr> 
+                      
+                      <th class ="block_time"> <?php echo $begin_hour?> - <?php echo $begin_hour+1;?>:00 </th>
+
+                      <?php foreach($week_days as $day){ ?> <!-- Para cada coluna -->
+                        <th class ="mark_appointment"> 
+                          <?php canMakeAppointment($schedule, $begin_hour, $day); ?>
+                        </th>
+                      <?php }?>
+                      
+                    </tr>
+
+                  <?php }?>
+                  
+
+            </table>
+          </div>
+
+        </section> 
